6008ff31ec77aec5361806c643c72fabdb4b00ac
saurabh-dhote-git-hub <saurabhdhotegithub@gmail.com>
2025-07-16 19:04:40 +0530

    update application properties, enhance AuthController logging, and adjust pom.xml dependencies

--- src/main/java/com/ssd/auth_service/controller/AuthController.java
+++ src/main/java/com/ssd/auth_service/controller/AuthController.java
@@ -5,7 +5,13 @@ import com.ssd.auth_service.dto.AuthResponse;
 import com.ssd.auth_service.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,8 +20,11 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    private int count = 1;
+
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) throws UnknownHostException {
+        System.out.printf("Call: %d - %s \n", count++, LocalDateTime.now());
         return ResponseEntity.ok(authService.login(request));
     }
 }
